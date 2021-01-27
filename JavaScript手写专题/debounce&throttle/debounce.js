823b79ee086b817bad9212d3bd77295d5e82d185
xingzheganlumei <xingzheganlumei@163.com>
2021-01-27 11:24:06 +0800

    feature: add throttle fn

--- JavaScript手写专题/debounce&throttle/debounce.js
+++ JavaScript手写专题/debounce&throttle/debounce.js
@@ -1,5 +1,6 @@
 /**
  * 高频函数设置防抖功能后，函数触发后会延迟n秒执行，在n秒内若函数再次被触发，则重新计时。
+ * 防抖又分立即执行和延迟执行两种
  * @param {Function} func 防抖函数
  * @param {Number} wait 间隔时间
  * @param {Boolean} immediate 立即执行函数
@@ -13,10 +14,10 @@ function debounce(func, wait, immediate) {
       clearTimeout(timer);
     }
     if (immediate) {
-      let callNow = !timer;
+      let callNow = !timer;// 第一次调用时，立即执行。否则则需等到两次调用间隔大于wait时才执行。
       timer = setTimeout(() => {
         timer = null;
-      });
+      }, wait);
       if (callNow) {
         result = func.apply(this, arguments);
       }
@@ -28,6 +29,7 @@ function debounce(func, wait, immediate) {
     return result;
   }
 
+  // 取消因防抖功能导致的延迟时长限制
   debounced.cancel = () => {
     clearTimeout(timer);
     timer = null;
---
+++ JavaScript手写专题/debounce&throttle/throttle.js
@@ -0,0 +1,107 @@
+/**
+ * 节流：
+ * 持续触发的函数，在n秒内只执行一次。
+ * 
+ * 具体又分为：第一次立即执行，和结束后再执行一次
+ */
+function throttle(func, wait, options) {
+  let timeoutId, context, args;
+  let previous = 0;
+  let options = options || {};
+
+  const getNowTimeStamp = () => (Date.now() || new Date().getTime());
+
+  const later = () => {
+    const now = getNowTimeStamp();
+    timeoutId = null;
+    previous = options.leading === false ? 0 : now;
+    func.apply(context, args);
+    context = args = null;
+  }
+
+  const throttle = function () {
+    context = this;
+    args = arguments;
+    const now = getNowTimeStamp();
+    if (!previous && options.leading === false) {
+      previous = now;
+    }
+    const remaining = wait - (now - previous);
+
+    if (remaining <= 0 || remaining > wait) {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+        timeoutId = null;
+      }
+      previous = now;
+      func.apply(context, args);
+      // 置为空是为了 js 的垃圾回收机制
+      if (!timeout) context = args = null;
+    } else if (!timeoutId && options.trailing !== false) {
+      timeoutId = setTimeout(later, remaining);
+    }
+  }
+
+  return throttle;
+}
+
+// 结束后再执行一次
+// function throttle(fn, wait) {
+//   let timer = null;
+
+//   return function() {
+//     if(!timer) {
+//       timer = setTimeout(() => {
+//         fn.apply(this, arguments);
+//         timer = null;
+//       }, wait);
+//     }
+//   }
+// }
+
+// // 第一次立即执行
+// function throttle(fn, wait) {
+//   let previous = 0;
+
+//   return function () {
+//     let now = Date.now() || new Date.getTime();
+//     if (now - previous > wait) {
+//       fn.apply(this, arguments);
+//       previous = now;
+//     }
+//   }
+// }
+
+// 双剑合璧, 通过 options 配置决定第一次是否立即执行以及结束后是否再执行一次。
+// function throttle(fn, wait, options) {
+//   let timer, context, args;
+//   let previous = 0;
+
+//   const getNowTimeStamp = () => (Date.now() || new Date().getTime());
+
+//   const later = () => {
+//     previous = getNowTimeStamp();
+//     timer = null;
+//     fn.apply(context, args);
+//   }
+
+//   const throttle = function () {
+//     context = this;
+//     args = arguments;
+//     let now = getNowTimeStamp();
+//     let remaining = wait - (now - previous);
+
+//     if (remaining <= 0 || remaining > wait) {
+//       if (timer) {
+//         clearTimeout(time);
+//         timer = null;
+//       }
+//       previous = now;
+//       fn.apply(context, args);
+//     } else if(!timer){
+//       setTimeout(later, remaining);
+//     }
+//   }
+
+//   return throttle;
+// }
