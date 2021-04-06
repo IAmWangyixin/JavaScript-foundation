06c13acec613685862371105bea9ce31803bfbce
xingzheganlumei <xingzheganlumei@163.com>
2021-04-06 16:04:40 +0800

    feature: 添加promise的all、race以及allSettled实现

---
+++ JavaScript手写专题/Promise/all&race&allSettled.js
@@ -0,0 +1,57 @@
+function all(iterator) {
+  let count = 0;
+  let elementCount = 0;
+  let result = [];
+  let anErrorOccurred = false;
+  return new Promise((resolve, reject) => {
+    for (let item of iterator) {
+      const currentIndex = count;//保证结果与 iterator 中的 promise 顺序一致。
+      Promise.resolve(item)
+        .then(data => {
+          if (anErrorOccurred) return;
+          result[currentIndex] = data;
+          elementCount++;
+          if (elementCount === iterator.length) {
+            resolve(result);
+          }
+        })
+        .catch(e => {
+          if (anErrorOccurred) return;
+          anErrorOccurred = true;
+          reject(e);
+        });
+      count++;
+    }
+    if(count === 0) {
+      resolve([]);
+    }
+  });
+}
+
+// test
+/* all([
+  new Promise(
+  (resolve) => setTimeout(
+    () => resolve(1),
+  1000)), 2, 3]) 
+*/
+
+function race(iterator) {
+  let occurred = false;
+  return new Promise((resolve, reject) => {
+    for (let item of iterator) {
+      Promise.resolve(item)
+        .then(data => {
+          if (occurred) return;
+          occurred = true;
+          resolve(data);
+        })
+        .catch(e => {
+          if (occurred) return;
+          occurred = true;
+          reject(e);
+        });
+      count++;
+    }
+  });
+}
